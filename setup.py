519762371b74da912e0f4e1f398052c9958f44a9
Eero af Heurlin <eero.afheurlin@iki.fi>
2017-08-18 12:30:49 +0300

    Fix installation with github deps

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ setup(
     license='MIT',
     long_description=open('README.md').read(),
     description='GoPro Metadata Format parser',
-    install_requires=open(os.path.join(os.path.dirname(__file__), 'requirements.txt'), 'rt').readlines(),
+    #install_requires=open(os.path.join(os.path.dirname(__file__), 'requirements.txt'), 'rt').readlines(),
+    install_requires=['construct==2.8.12', 'python-dateutil==2.6.1', 'hachoir3==3.0a2'],
+    dependency_links=['https://github.com/rambo/hachoir3/archive/hotfix_302a_EditableTimestampMac32.zip#egg=hachoir3-3.0a2',],
     url='https://github.com/rambo/python-gpmf',
 )
